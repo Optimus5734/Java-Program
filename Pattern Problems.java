69db3436ca1ed8daa84ba4b756b7011ea521d2e2
Sagar Verma <105728547+Optimus5734@users.noreply.github.com>
2022-09-27 00:18:01 +0530

    Update Pattern Problems.java

--- Pattern Problems.java
+++ Pattern Problems.java
@@ -109,3 +109,19 @@
         }
     }
 }
+
+9.
+class Solution9 {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = i ; j >= 1 ; j-- ){
+                    System.out.print(j%2+" ");
+                    
+                }
+                System.out.println();
+                
+            }
+        }
+    }
