5ba37d5f5dbdf60c08a7991b5d7a598e7b9055c7
Sagar Verma <105728547+Optimus5734@users.noreply.github.com>
2022-09-30 20:26:10 +0530

    Update Pattern Problems.java

--- Pattern Problems.java
+++ Pattern Problems.java
@@ -199,3 +199,72 @@ class Solution11 {
         
     }
 }
+14.
+class Solution14 {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i = 0 ; i<n ; i++){
+            for(int j = n ; j >  i ; j--)
+                System.out.print("*");
+            
+            for(int j = 0 ; j<i ; j++)
+               System.out.print("  ");
+               
+            for(int j = n ; j >  i ; j--)
+                System.out.print("*");
+            
+            System.out.println();
+        }
+        
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j <=  i ; j++)
+                System.out.print("*");
+            
+            for(int j = n-1 ; j>i ; j--)
+               System.out.print("  ");
+               
+            for(int j = 0 ; j <=  i ; j++)
+                System.out.print("*");
+            
+            System.out.println();
+        }
+    }
+}
+
+15.class Solution15 {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i = 0 ; i<n ; i++){
+            
+            for(int j = 0 ; j <=  i ; j++)
+                System.out.print("*");
+                
+            
+            for(int j = n-1 ; j>i ; j--)
+               System.out.print("  ");
+               
+            for(int j = 0 ; j <=  i ; j++)
+                System.out.print("*");
+                
+            
+            
+            System.out.println();
+        }
+        
+        for(int i = 0 ; i<n-1 ; i++){
+            
+            for(int j = n-1 ; j >  i ; j--)
+                System.out.print("*");
+                
+            for(int j = 0 ; j<=i ; j++)
+               System.out.print("  ");
+               
+            for(int j = n-1 ; j >  i ; j--)
+                System.out.print("*");
+            
+            System.out.println();
+        }
+    }
+}
